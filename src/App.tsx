d7d9e7cdbf1f153a8acb6bd2ae082ee449bc630f
Marc Löhe <loehe@sipgate.de>
2025-02-28 10:12:01 +0100

    chore: add prettier and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,85 @@
-import {Bar} from "react-chartjs-2";
-import {BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip,} from 'chart.js';
-import {faker} from '@faker-js/faker';
-import {Panel} from "./Panel.tsx";
+import { Bar } from "react-chartjs-2";
+import {
+	BarElement,
+	CategoryScale,
+	Chart as ChartJS,
+	Legend,
+	LinearScale,
+	Title,
+	Tooltip,
+} from "chart.js";
+import { faker } from "@faker-js/faker";
+import { Panel } from "./Panel.tsx";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
 );
 
 function App() {
+	const options = {
+		indexAxis: "y" as const,
+		elements: {
+			bar: {
+				borderWidth: 2,
+			},
+		},
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "right" as const,
+			},
+			title: {
+				display: true,
+				text: "Chart.js Horizontal Bar Chart",
+			},
+		},
+	};
 
-    const options = {
-        indexAxis: 'y' as const,
-        elements: {
-            bar: {
-                borderWidth: 2,
-            },
-        },
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'right' as const,
-            },
-            title: {
-                display: true,
-                text: 'Chart.js Horizontal Bar Chart',
-            },
-        },
-    };
+	const labels = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+	];
 
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+	const data = {
+		labels,
+		datasets: [
+			{
+				label: "Dataset 1",
+				data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+				borderColor: "rgb(255, 99, 132)",
+				backgroundColor: "rgba(255, 99, 132, 0.5)",
+			},
+			{
+				label: "Dataset 2",
+				data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+				borderColor: "rgb(53, 162, 235)",
+				backgroundColor: "rgba(53, 162, 235, 0.5)",
+			},
+		],
+	};
 
-    const data = {
-        labels,
-        datasets: [
-            {
-                label: 'Dataset 1',
-                data: labels.map(() => faker.number.int({min: -1000, max: 1000})),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            },
-            {
-                label: 'Dataset 2',
-                data: labels.map(() => faker.number.int({min: -1000, max: 1000})),
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            },
-        ],
-    };
-
-    return (
-        <>
-            <header>
-                <h1>Analyse</h1>
-            </header>
-            <main>
-                <Panel title={"Verteilung der Anrufe"}>
-                    <Bar options={options} data={data}/>
-                </Panel>
-            </main>
-        </>
-    )
+	return (
+		<>
+			<header>
+				<h1>Analyse</h1>
+			</header>
+			<main>
+				<Panel title={"Verteilung der Anrufe"}>
+					<Bar options={options} data={data} />
+				</Panel>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/Panel.tsx
+++ src/Panel.tsx
@@ -2,17 +2,17 @@ import * as React from "react";
 import styles from "./Panel.module.css";
 
 type Props = {
-    title: string;
-    children: React.ReactNode;
-}
+	title: string;
+	children: React.ReactNode;
+};
 
-export const Panel: React.FC<Props> = ({title, children}) => {
-    return (
-        <div className={styles.wrapper}>
-            <div>
-                <h2>{title}</h2>
-            </div>
-            {children}
-        </div>
-    )
-}
+export const Panel: React.FC<Props> = ({ title, children }) => {
+	return (
+		<div className={styles.wrapper}>
+			<div>
+				<h2>{title}</h2>
+			</div>
+			{children}
+		</div>
+	);
+};
